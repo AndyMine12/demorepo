34ecd9154107d935539aad756d594021923be3d1
Victor Madera <72475885+Klaz1k@users.noreply.github.com>
2024-10-19 11:21:19 -0400

    Fixed model for SqlModel ORM

--- practica3/Repository.py
+++ practica3/Repository.py
@@ -1,23 +1,24 @@
-from sqlmodel import Field, Session, SQLModel
+from sqlmodel import Session
+from directory import Directory
 
 class DirectoryRepository():
   def __init__(self):
     pass
   
-  def get_count(self):
+  def get_count(self) -> int:
     pass
 
-  def find_all(self):
+  def find_all(self, page: int, perpage: int) -> list[Directory]:
     pass
 
-  def find_by_id(self, id: int):
+  def find_by_id(self, id: int) -> Directory:
     pass
 
-  def create(self, name: str, emails: list[str]):
+  def create(self, name: str, emails: list[str]) -> Directory:
     pass
 
-  def update(self, id: int, name: (str | None) = None, emails: (list[str] | None) = None):
+  def update(self, id: int, name: (str | None) = None, emails: (list[str] | None) = None) -> Directory:
     pass
 
-  def delete(self, id: int):
+  def delete(self, id: int) -> bool:
     pass
--- practica3/directory.py
+++ practica3/directory.py
@@ -1,4 +1,12 @@
-class Directory:
-  def __init__(self, name: str, emails: list[str]) -> None:
-    self.name = name
-    self.emails = emails
+from sqlmodel import Field, SQLModel, Column, Relationship
+
+class Directory(SQLModel, table=True):
+  id: int | None = Field(default=None,  primary_key=True)
+  name: str = Field(sa_column=Column(nullable=False))
+  emails: list["Email"] = Relationship(back_populates="directory")
+
+class Email(SQLModel, table=True):
+  id: int | None = Field(default=None, primary_key=True)
+  content: str = Field(sa_column=Column(nullable=False))
+  directory_id: int | None = Field(default=None, foreign_key="directory.id")
+  directory: Directory | None = Relationship(back_populates="emails")
